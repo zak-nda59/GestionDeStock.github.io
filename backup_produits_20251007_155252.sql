25f8ce1f527a8c264cb2b1ab9b06a41ad57441a0
zak-nda59 <abdelliz484@gmail.com>
2025-10-07 16:52:40 +0200

    Delete backup_produits_20251007_155252.sql

--- backup_produits_20251007_155252.sql
+++
@@ -1,7 +0,0 @@
--- Sauvegarde automatique de la base de données
--- Date: 2025-10-07 15:52:52
-
-DELETE FROM produits;
-ALTER TABLE produits AUTO_INCREMENT = 1;
-
-INSERT INTO produits (nom, code_barres, prix, stock) VALUES ('produit 1', '789525', 30.00, 2);
